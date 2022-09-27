08d5608ee525bb89552c2f5b1bfe2875f72be80b
PC user <aslanyan.maksim@gmail.com>
2022-09-28 01:47:53 +0400

    Initial commit

---
+++ src/main/java/com/example/companyemploeespring/CompanyEmploeeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.companyemploeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyEmploeeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyEmploeeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/companyemploeespring/CompanyEmploeeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyemploeespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyEmploeeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
